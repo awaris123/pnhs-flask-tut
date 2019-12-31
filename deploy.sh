9c9f224a0564ff8e5679863a1e2c851ebf583eea
Aakef Waris <awaris@hawk.iit.edu>
2019-12-31 10:25:47 -0600

    changed heroku deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
+if [ $# -eq 0 ]
+	then
+		MSG = $1
+	else
+		MSG = "Trigger Re-Deploy"
+fi
+
 git add .
-git commit --allow-empty -m "Trigger Re-Deploy"
+git commit --allow-empty -m MSG
 git push heroku flask-hero-api:master
 
